e87b003fe86b0bbde19af49544a3e1a887715b6c
Sol A <johnksy12@gmail.com>
2016-05-26 00:39:22 +0900

    revise data, type

--- data/sql/GetEat_DDL.sql
+++ data/sql/GetEat_DDL.sql
@@ -2,8 +2,8 @@
     Server
 */
 create table restaurant(
-    Rest_Name   varchar(30) primary key,
-    Category    varchar(30) not null,
+    Rest_Name   varchar(50) primary key,
+    Category    varchar(50) not null,
     Location    varchar(30),
     Number  varchar(30),
     Hours_Start varchar(30),
@@ -13,10 +13,10 @@ create table restaurant(
 	URL			varchar(150)
 );
 create table menu(
-    Rest_Name   varchar(30),
+    Rest_Name   varchar(50),
     Menu_ID     varchar(10),
-    Menu_Name   varchar(50) not null,
-    Category    varchar(50) not null,
+    Menu_Name   varchar(120) not null,
+    Category    varchar(100) not null,
     Price   varchar(10) not null,
     Frequency   int,
     primary key (Menu_ID),
@@ -24,14 +24,14 @@ create table menu(
 );
 
 create table review(
-    Rest_name varchar(30),
-    Score   varchar(1),
+    Rest_name varchar(50),
+    Score   varchar(5),
     People  int,
     foreign key (Rest_name) references restaurant
 );
 
 create table users(
-    User_ID varchar(10) primary key,
+    User_ID varchar(30) primary key,
     Name    varchar(30),
     Phone   varchar(30),
     Account varchar(30),
@@ -39,20 +39,20 @@ create table users(
 );
 
 create table party(
-    Party_Name  varchar(30),
+    Party_Name  varchar(50),
     Leader_ID   varchar(30),
     Sustainability  varchar(30),
-    User_ID     varchar(10),
-    Location    varchar(10),
+    User_ID     varchar(30),
+    Location    varchar(30),
     Price       varchar(10),
     primary key (Party_Name, Leader_ID)
 );
 create table ordered(
-    Order_ID varchar(10) primary key,
+    Order_ID varchar(30) primary key,
     Date    varchar(30),
     Name    varchar(30),
-    Leader_ID   varchar(10),
-    Rest_Name   varchar(30),
+    Leader_ID   varchar(30),
+    Rest_Name   varchar(50),
     Menu_ID     varchar(10),
     foreign key (Rest_Name) references restaurant
     );
@@ -60,17 +60,18 @@ create table ordered(
 /*
     Local
 */
+
 create table history(
     Date    varchar(30),
-    Rest_Name varchar(30),
+    Rest_Name varchar(50),
     Price   varchar(10),
-    Score   varchar(1),
+    Score   varchar(5),
     primary key (Date, Rest_Name)
 );
 create table friend(
-    User_ID varchar(10) primary key,
-    Name    varchar(10),
-    Account varchar(15),
-    Bank    varchar(10),
+    User_ID varchar(30) primary key,
+    Name    varchar(30),
+    Account varchar(30),
+    Bank    varchar(30),
     Dept    varchar(10)
 );
